1c9a935a53e0a9c62dbe9159eaa7b37ac81d6d4f
Sudhanshu <bsudhanshu3@gmail.com>
2022-02-20 15:59:54 +0530

    authMiddleware setup

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middleware/auth");
 const { login, dashboard } = require("../controllers/main");
 
-router.route("/dashboard").get(dashboard);
+// router.use(authMiddleware)   // use this if we want to use the middleware for all the below routes in this file.
+
+router.route("/dashboard").get(authMiddleware, dashboard);
 router.route("/login").post(login);
 
 module.exports = router;
